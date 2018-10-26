4674160cd7a4b859cefd35ffba53e075792ed350
Manuel Espinoza <mfewcar@gmail.com>
2018-10-25 23:12:54 -0600

    Unit 5 WIP struggling with displaying image datainfo

--- Unit05/saveToDatabasePractice.php
+++ Unit05/saveToDatabasePractice.php
@@ -6,11 +6,12 @@ $department = $_POST[dept];
 $phone = $_POST[phone];
 $photo = $_POST[photo];
 
-// make photo path and name
-$ext = pathinfo( ,PATHINFO_EXTENSION);
+// // make photo path and name
+$ext = pathinfo(PATHINFO_EXTENSION);
 
 
-$filename = 'employees'
+$filename = 'employees/missing.jpg';
+echo $filename;
 
 
 
@@ -24,19 +25,21 @@ $filename = 'employees'
 
 
 
-<!DOCTYPE <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Page Title</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="styles.css" />
   <script src="main.js"></script>
 </head>
 <body>
   <h1>Test Upload Page</h1>
   <?php
+
+  echo '<img src="'.$filename.'" alt="photo"/>';
 
   echo '<br>size--'.$_FILES['photo']['size'];
   echo '<br>type--'.$_FILES['photo']['type'];
