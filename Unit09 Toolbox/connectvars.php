97e03dee37941fb14778d13dbbb7904560d105ec
manoy96 <mfewcar@gmail.com>
2019-03-22 18:45:52 -0600

    edit to connection

--- Unit09 Toolbox/connectvars.php
+++ Unit09 Toolbox/connectvars.php
@@ -1,7 +1,7 @@
 <?php 
-define('HOST', 'localhost');
-define('USER','manuele1_3760usr');
-define('PASSWORD','y(-aJt=?#-!J');
+define('DB_HOST', 'localhost');
+define('DB_USER','manuele1_3760usr');
+define('DB_PASSWORD','y(-aJt=?#-!J');
 define('DB_NAME','manuele1_3760test');
 
 
