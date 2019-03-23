44577343efecdfd3d39e1ac98f27b2ea670ca210
manoy96 <mfewcar@gmail.com>
2019-03-22 22:07:08 -0600

    setting up Unit 8

--- Unit08 Table Joins/footer.php
+++
@@ -1,5 +0,0 @@
-<div id="footer">
-    
-<h3>©2018 Manuel Espinoza</h3>
-   
-</div>
--- Unit08 Table Joins/protect.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //MAKE SURE THE USER IS LOGGED IN BEFOR GOING ANY FURTHER
-  if (!isset($_COOKIE['username'])) {
-    echo '<p class="login">Please <a href="login.php">Log in</a> to access this page</p>';
-    exit();
-  }//end IF
-?>
