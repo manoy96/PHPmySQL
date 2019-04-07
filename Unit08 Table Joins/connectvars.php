a182e1dc544c02ec06219b35e269c832f34a2a67
manoy96 <mfewcar@gmail.com>
2019-04-06 22:43:45 -0600

    working on getting Unit 8 working

--- Unit08 Table Joins/connectvars.php
+++ Unit08 Table Joins/connectvars.php
@@ -1,15 +1,7 @@
-<?php 
-define('DB_HOST', 'localhost');
-define('DB_USER','manuele1_3760usr');
-define('DB_PASSWORD','y(-aJt=?#-!J');
-define('DB_NAME','manuele1_3760test');
-
-
-
-
-
-
-
-
-
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'localhost');
+  define('DB_USER','manuele1_3760usr');
+  define('DB_PASSWORD','y(-aJt=?#-!J');
+  define('DB_NAME','manuele1_3760test');
 ?>
--- Unit08 Table Joins/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-<p class="footer">Copyright &copy;2018 MatchMaker, Inc.</p><img class="social" src="social-media-icons.png" alt="Social Media Links" />
-</footer>
-</body>
-</html>
