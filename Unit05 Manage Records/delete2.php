e041dbcb4337225e1a6f58787bfdf9397aab11e7
manoy96 <mfewcar@gmail.com>
2019-04-10 10:32:22 -0600

    fixed Delete 2 for unit 5

--- Unit05 Manage Records/delete2.php
+++ Unit05 Manage Records/delete2.php
@@ -1,45 +1,74 @@
 <?php 
+
 $employee_id = $_GET[id];
-
-//BUILD THE DATABASE CONNECTIONWITH host, user, pass, database
+// BUILD THE DATABASE CONNECTION WITH host, user, pass, database
 $dbconnection = mysqli_connect('localhost','manuele1_3760usr','y(-aJt=?#-!J','manuele1_3760test') or die('connection failed');
-
-//DELETE SELECTED RECORD (IN FROM POST)
-if (isset($_POST['submit'])) {
-  // echo $_POST['id'];
-  // echo $_POST['photo'];
-
-  //BUILD A SELECT QUERY
-  $query = "DELETE FROM employee_simple WHERE id=$_POST[id]";
-  // echo $query;
-
-  //NOW TRY AND DELETE THE RECORD
-  $result = mysqli_query($dbconnection ,$query) or die('delete query failed');
-
-  // echo $_POST['photo'];
-  //delete the image
-  @unlink($_POST['photo']);
-
-  //REDIRECT
-  header("Location: ");
-
-
-  // MAKE SURE CODE BELOW DOES NOT GET EXECUTED WHEN WE REDIRECT
-  exit;
+// DELETE SELECTED RECORD (ID FROM POST)
+if(isset($_POST['submit'])) {
+    
+    // BUILD A SELECT QUERY
+    $query = "DELETE FROM employee_simple WHERE id=$_POST[id]";
+    
+    // NOW TRY AND DELETE THE RECORD
+    $result = mysqli_query($dbconnection, $query) or die ("Delete2 query failed");
+    
+    // delete the image
+    @unlink($_POST['photo']);
+    
+    // REDIRECT
+    header("Location: http://dgm3760.manuelespin.com/Unit05%20Manage%20Records/delete.php");
+    
+    // END CODE WHEN WE REDIRECT
+    exit;
 };
-
-
-
-
-
-//DISPLAY SELECTED RECORDS
-$query = "SELECT * FROM employee_simple WHERE id=employee_id";
-
-// //NOW TRY AND TALK TO THE DATABASE
-$result = mysqli_query($dbconnection ,$query) or die('Talk query failed');
-
-//PUT RESULTS IN A VARIABLE
+// BUILD THE SELECT QUERY
+$query = "SELECT * FROM employee_simple WHERE id=$employee_id";
+// NOW TRY AND TALK TO THE DATABASE
+$result = mysqli_query($dbconnection, $query) or die ("Query Failed");
+// PUT THE RESULTS IN A VARIABLE
 $found = mysqli_fetch_array($result);
+//-------------------
+// $employee_id = $_GET[id];
+
+// //BUILD THE DATABASE CONNECTIONWITH host, user, pass, database
+// $dbconnection = mysqli_connect('localhost','manuele1_3760usr','y(-aJt=?#-!J','manuele1_3760test') or die('connection failed');
+
+// //DELETE SELECTED RECORD (IN FROM POST)
+// if (isset($_POST['submit'])) {
+//   // echo $_POST['id'];
+//   // echo $_POST['photo'];
+
+//   //BUILD A SELECT QUERY
+//   $query = "DELETE FROM employee_simple WHERE id=$_POST[id]";
+//   // echo $query;
+
+//   //NOW TRY AND DELETE THE RECORD
+//   $result = mysqli_query($dbconnection ,$query) or die('delete2 query failed');
+
+//   // echo $_POST['photo'];
+//   //delete the image
+//   @unlink($_POST['photo']);
+
+//   //REDIRECT
+//   header("Location: ");
+
+
+//   // MAKE SURE CODE BELOW DOES NOT GET EXECUTED WHEN WE REDIRECT
+//   exit;
+// };
+
+
+
+
+
+// //DISPLAY SELECTED RECORDS
+// $query = "SELECT * FROM employee_simple WHERE id=employee_id";
+
+// // // //NOW TRY AND TALK TO THE DATABASE
+// $result = mysqli_query($dbconnection ,$query) or die('Talk query failed');
+
+// //PUT RESULTS IN A VARIABLE
+// $found = mysqli_fetch_array($result);
 ?>
 
 
@@ -58,7 +87,7 @@ $found = mysqli_fetch_array($result);
   <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
 </head>
 <body>
-  <h1>Deleting an Employee</h1>
+  <h1 class="delete2" >Deleting an Employee</h1>
 
   <div class="employee">
     <form action="delete2.php" method="POST">
